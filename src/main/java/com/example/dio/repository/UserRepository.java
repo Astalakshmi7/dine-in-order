ab0458d9386fd35bd335df394751f9dd83d844e1
Astalakshmi7 <astalakshmiarivu@gmail.com>
2025-02-27 14:37:57 +0530

    modifying the userRepository

--- src/main/java/com/example/dio/repository/UserRepository.java
+++ src/main/java/com/example/dio/repository/UserRepository.java
@@ -4,6 +4,5 @@ import com.example.dio.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface "UserRepository extends JpaRepository<User,Long> {
-
+public interface UserRepository extends JpaRepository<User,Long> {
 }
