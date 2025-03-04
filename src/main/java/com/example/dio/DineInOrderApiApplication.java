4347228671cc36199d0f53a033c0079baae57fa8
Astalakshmi7 <astalakshmiarivu@gmail.com>
2025-03-04 13:12:10 +0530

    modifying file

--- src/main/java/com/example/dio/DineInOrderApiApplication.java
+++ src/main/java/com/example/dio/DineInOrderApiApplication.java
@@ -2,8 +2,12 @@ package com.example.dio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+
+
 public class DineInOrderApiApplication {
 
 	public static void main(String[] args) {
